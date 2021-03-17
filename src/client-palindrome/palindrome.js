ac651e9aa3295b33ba1121a032107511db67a79a
Tom <thomas.spencer@codurance.com>
2021-03-17 23:40:43 +0000

    Add passing tests for palindrome

---
+++ src/client-palindrome/palindrome.js
@@ -0,0 +1,11 @@
+const isPalindrome = phrase => {
+  if (phrase === undefined) {
+    throw new Error('Invalid argument');
+  }
+
+  return (
+    phrase.trim().length > 0 && phrase.split('').reverse().join('') === phrase
+  );
+}
+
+module.exports = isPalindrome;
