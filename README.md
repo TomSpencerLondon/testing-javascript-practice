55ec1ac263259a7935e2503587f1210f0438b207
Tom <thomas.spencer@codurance.com>
2021-03-18 00:57:36 +0000

    add tets

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/Dan-Bird/sandbox-testing-javascript-applications
---
+++ test/locate-me/onerror.test.js
@@ -0,0 +1,18 @@
+
+const { onError } = require('../../src/locateme/locateme');
+
+describe('onError test', () => {
+  it('should set the error DOM element', () => {
+    const domElement = { innerHTML: '' };
+    const message = "you're kidding";
+    const positionError = { message: message };
+
+    jest
+      .spyOn(document, 'getElementById')
+      .mockImplementation(elementId => domElement);
+
+    onError(positionError);
+
+    expect(domElement.innerHTML).toBe(message);
+  });
+});
